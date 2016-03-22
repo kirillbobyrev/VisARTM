81387309a77230101bdaccb8d405688e30a2c1c5
Kirill Bobyrev <kbobyrev@hotmail.com>
2016-03-23 01:13:44 +0300

    transferring latest progress to MacBook

--- views.py
+++ views.py
@@ -50,10 +50,6 @@ def topic(dataset_id, topicmodel_id, topic_id):
     topic_terms = TopicTerm.query.filter_by(topicmodel=topicmodel_id,
         topic=topic_id)
     terms = Term.query.filter(id in [tt.term for tt in topic_terms.all()])
-    document_topics = DocumentTopic.query.filter_by(topicmodel=topicmodel_id,
-        topic=topic_id)
-    documents = Document.query.filter(
-        id in [dt.document for dt in document_topics.all()])
 
     terms_wt_table = Table(
         heading='Terms sorted by p(w|t)',
@@ -71,6 +67,39 @@ def topic(dataset_id, topicmodel_id, topic_id):
             for tt in topic_terms.order_by('prob_tw')[::-1]]
     )
 
+    document_topics = DocumentTopic.query.filter_by(topicmodel=topicmodel_id,
+        topic=topic_id)
+    documents = Document.query.filter(
+        id in [dt.document for dt in document_topics.all()])
+
+    documents_dt_table = Table(
+        heading='Documents sorted by p(d|t)',
+        column_names=['p(d|t)', 'Document'],
+        rows=[[dt.prob_dt, url_for(Document, dataset_id, topicmodel_id, dt.document),
+            Document.query.get((dt.document, dataset_id)).title]
+            for dt in document_topics.order_by('prob_dt')[::-1]]
+    )
+
+    documents_td_table = Table(
+        heading='Documents sorted by p(d|t)',
+        column_names=['p(d|t)', 'Document'],
+        rows=[[dt.prob_dt, 
+            url_for(Document, dataset_id, topicmodel_id, dt.document),
+            Document.query.get((dt.document, dataset_id)).title]
+            for dt in document_topics.order_by('prob_dt')[::-1]]
+    )
+
+    topic_similarities = TopicSimilarity.query.filter_by(
+        topicmodel=topicmodel_id, topic_l=topic_id)
+    topic_similarities_table = Table(
+        heading='Topic Similarities',
+        column_names=['Similarity', 'Topic'],
+        rows=[[ts.similarity, 
+            url_for(Topic, dataset_id, topicmodel_id, ts.topic_r),
+            Topic.query.get((topicmodel_id, ts.topic_r)).title]
+            for ts in topic_similarities.order_by('similarity')[::-1]]
+    )
+
     return render_template('topic.html',
         dataset=Dataset.query.get(dataset_id),
         topicmodel=TopicModel.query.get((dataset_id, topicmodel_id)),
@@ -78,7 +107,8 @@ def topic(dataset_id, topicmodel_id, topic_id):
         terms_wt_table=terms_wt_table,
         terms_tw_table=terms_tw_table,
         documents_dt_table=documents_dt_table,
-        documents_td_tbale=documents_td_table)
+        documents_td_table=documents_td_table,
+        topic_similarities_table=topic_similarities_table)
 
 
 @app.route('/dataset/<int:dataset_id>/topic_model/<int:topicmodel_id>/'
